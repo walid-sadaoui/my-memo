655e09241a29994989cce0417a2a55de67cff11a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-28 10:29:15 +0000

    Deploy walid-sadaoui/my-memo to github.com/walid-sadaoui/my-memo.git:gh-pages

---
+++ precache-manifest.f13b356c38cefca65befc5b61d3517d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299b4f1491decb06de5c1b9a407d3750",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbb0328dc61fe5fc5b57",
+    "url": "/static/css/main.e6ee7588.chunk.css"
+  },
+  {
+    "revision": "2ad870491518399b1cf5",
+    "url": "/static/js/2.5aab8c24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5aab8c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb0328dc61fe5fc5b57",
+    "url": "/static/js/main.e137de5c.chunk.js"
+  },
+  {
+    "revision": "16fec5d496fdb0c9f7f5",
+    "url": "/static/js/runtime-main.e37d8aa1.js"
+  },
+  {
+    "revision": "f25663f75b750c7f7537dea9e351c5e0",
+    "url": "/static/media/cog.f25663f7.svg"
+  },
+  {
+    "revision": "35fce789c65bbb156b6229beb30e0d6e",
+    "url": "/static/media/download.35fce789.svg"
+  }
+]);
