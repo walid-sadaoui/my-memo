45608745ddaa99bed6460a08ccd32d5d8074754a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-11 17:03:52 +0000

    Deploy walid-sadaoui/my-memo to github.com/walid-sadaoui/my-memo.git:gh-pages

---
+++ precache-manifest.7089f0bb47166c1a00988b6dd7429d56.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f178fba79880f6500cfa01ef07ebc12b",
+    "url": "/my-memo/index.html"
+  },
+  {
+    "revision": "0edb7d7d2de6f18eb6a3",
+    "url": "/my-memo/static/css/main.02a5fc67.chunk.css"
+  },
+  {
+    "revision": "abc7afbf27910abc59f6",
+    "url": "/my-memo/static/js/2.4b5d3b96.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-memo/static/js/2.4b5d3b96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edb7d7d2de6f18eb6a3",
+    "url": "/my-memo/static/js/main.d0e52ecb.chunk.js"
+  },
+  {
+    "revision": "646f671abf47dd356a96",
+    "url": "/my-memo/static/js/runtime-main.b66b2661.js"
+  },
+  {
+    "revision": "a66f1a4eef3c51e32a318858633c3410",
+    "url": "/my-memo/static/media/check.a66f1a4e.svg"
+  },
+  {
+    "revision": "f25663f75b750c7f7537dea9e351c5e0",
+    "url": "/my-memo/static/media/cog.f25663f7.svg"
+  },
+  {
+    "revision": "35fce789c65bbb156b6229beb30e0d6e",
+    "url": "/my-memo/static/media/download.35fce789.svg"
+  },
+  {
+    "revision": "a33a3cec602cced021084654a1cade1e",
+    "url": "/my-memo/static/media/flag.a33a3cec.svg"
+  },
+  {
+    "revision": "f7210926414935f5006692dc71b8a43a",
+    "url": "/my-memo/static/media/pencil.f7210926.svg"
+  },
+  {
+    "revision": "3cee99ee47d1991bf6ffdb5ca01477ac",
+    "url": "/my-memo/static/media/plus.3cee99ee.svg"
+  },
+  {
+    "revision": "13a5d9d75e4b1d1061b7a8b6bf54160b",
+    "url": "/my-memo/static/media/trash.13a5d9d7.svg"
+  },
+  {
+    "revision": "dbbf69f1c4480b9da0480a6b33691130",
+    "url": "/my-memo/static/media/x.dbbf69f1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bfd1a9d389eff4075118501a131fd62c.js"
+  "/my-memo/precache-manifest.7089f0bb47166c1a00988b6dd7429d56.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-memo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
